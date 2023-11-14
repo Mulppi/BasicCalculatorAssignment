c76754f5b12100e3cd3634e7252cfaa1f3dceb00
Mulppi <clasher204player@gmail.com>
2023-11-14 12:48:22 +0200

    Assignment questions answered

--- BasicCalculatorAssignment/Program.cs
+++ BasicCalculatorAssignment/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace BasicCalculatorAssignment
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace BasicCalculatorAssignment
 {
     internal class Program
     {
@@ -18,18 +22,38 @@
                 Console.Write("Choose an operation: ");
 
                 // Task 1. Read choice input and assing it to new int variable
-
+                int choice = Convert.ToInt32(Console.ReadLine());
                 // Task 2. Check if user selected choice 5. If so then exit the loop
+                if (choice == 5)
+                {
+                    break;
+                }
 
                 Console.Write("Enter first number: ");
                 // Task 3. Get the first number and assing it to new double variable
-
+                double num1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Enter second number: ");
                 // Task 4. Get the second number and assing it to new double variable
+                double num2 = Convert.ToInt32(Console.ReadLine());
 
                 // Task 5. Create double variable for default result value and assign it to be 0
-
+                double defval = 0;
                 // Task 6. Handle choices using switch statement
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine(add(num1, num2));
+                        break;
+                    case 2:
+                        Console.WriteLine(subtract(num1, num2));
+                        break;
+                    case 3:
+                        Console.WriteLine(multiply(num1, num2));
+                        break;
+                    case 4:
+                        Console.WriteLine(divide(num1, num2));
+                        break;
+                }
                 // Task 6.1 For each case create new function/method an call it inside the case
                 //          - Case 1: Add 
                 //          - Case 2: Subtract
@@ -40,9 +64,29 @@
                 //       There is comment below where you can place your methods
 
                 // Task 7. Print out the result
+
+                // Code prints result in the switch
             }
         }
 
         // Declare your methods/functions here
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
+        static double subtract(double a, double b)
+        {
+            return a - b;
+        }
+        static double multiply(double a, double b)
+        {
+            return a * b;
+        }
+        static double divide(double a, double b)
+        {
+            if (b!=0)
+                return a / b;
+            return 0; // placeholder value if trying to divide by 0 
+        }
     }
 }
